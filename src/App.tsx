b10a927b582a0307793b166b01f188c124db6d20
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2022-01-23 19:47:27 -0500

    Created the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [gridArray, setGridArray] = useState<any>(Array(100).fill(true));
+  const [startPos, setStartPos] = useState({ x: 0, y: 0 });
+
+  // const elemBackgroundChange = (index:number) => {
+  //   const opp = !gridArray[index];
+  //   setGridArray((currGridArray:any) => {
+  //     currGridArray[index] = opp;
+  //     return [...currGridArray];
+  //   });
+  // }
+
+  const elemStartDrag = (e:React.MouseEvent, index: number) => {
+    e.preventDefault();
+    const pos = { x: Math.floor(index / 10), y: index % 10 };
+    setStartPos(pos);
+    console.log(index);
+    console.log(pos);
+  };
+
+  const elemEndDrag = (e:React.MouseEvent, index: number) => {
+    e.preventDefault();
+    const endPos = { x: Math.floor(index / 10), y: index % 10 };
+    let positions = { startX: startPos.x, endX: endPos.x, startY: startPos.y, endY: endPos.y };
+    if (endPos.x < startPos.x) {
+      positions.startX =  endPos.x;
+      positions.endX = startPos.x;
+    }
+    if (endPos.y < startPos.y) {
+      positions.startY =  endPos.y;
+      positions.endY = startPos.y;
+    }
+    for (let y = positions.startY; y <= positions.endY; y++) {
+      for (let x = positions.startX; x <= positions.endX; x++) {
+        console.log(x + " " + y);
+        const opp = !gridArray[x * 10 + y];
+        setGridArray((currGridArray: any) => {
+          currGridArray[x * 10 + y] = opp;
+          return [...currGridArray];
+        });
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="grid">
+        {gridArray.map((elem: any, index: number) => (
+          <div
+            key={index}
+            draggable="false"
+            onMouseDown={(e) => {
+              elemStartDrag(e, index);
+            }}
+            onMouseUp={(e) => elemEndDrag(e, index)}
+            /* onClick={() => elemBackgroundChange(index)} */
+          >
+            <div className={elem ? "grid-elem active" : "grid-elem"}></div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
